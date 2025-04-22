02e8c17ac4b1ecaa3fa639b7ea69fb5642d07cc0
Thibault LEFAY <thibault.lefay.pro@gmail.com>
2025-04-22 21:38:55 +0200

    modif

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,29 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-async function connectWithRetry() {
-  let connected = false;
-  let connection;
+const db = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-  while (!connected) {
-    try {
-      connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-      });
+console.log("✅ Connecté à la base de données MySQL");
 
-      console.log("✅ Connecté à la base de données MySQL");
-      connected = true;
-    } catch (err) {
-      console.error("❌ Connexion à MySQL échouée. Nouvelle tentative dans 3 secondes...");
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-    }
-  }
-
-  return connection;
-}
-
-const db = await connectWithRetry();
 export default db;
